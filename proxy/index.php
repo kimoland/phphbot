7ae7a82797d4109a9b6f969708e2f6f60e4bc63e
kimoland <85030146+kimoland@users.noreply.github.com>
2022-06-23 05:26:31 -0700

    Update index.php

--- proxy/index.php
+++ proxy/index.php
@@ -25,7 +25,7 @@ for ($i = 0; $i < count($json);$i++)
     $ip = $json[$i]["ip"];
     $port = $json[$i]["prt"];
     $secret = $json[$i]["secret"];
-    $message .= "https://t.me/proxy?server=$ip&port=$port&secret=$secret"."\n\n";
+    $message .= "🌐پروکسی $i "."https://t.me/proxy?server=$ip&port=$port&secret=$secret"."\n\n";
 }
 
 ?>
